6b01e0f1f1c2f5d0970a373a687774fcee8d711d
Kristi Dugan <kristi@dugan.ws>
2019-09-16 18:38:01 -0500

    Add Erase and Resize buttons

--- script.js
+++ script.js
@@ -6,18 +6,48 @@ const divMaker = num => {
 		container.appendChild(div);
 	}
 };
-divMaker(16);
+divMaker(16); // Create default grid
 
-const cells = document.querySelectorAll('main div');
+const etchPad = document.querySelector('main');
+let cells;
 
-cells.forEach( (cell) => {
-	cell.addEventListener('mouseenter', () => {
-		cell.classList.add('colored');
+const cellFinder = () => {
+	cells = document.querySelectorAll('main div');
+};
+
+const cellListener = () => {
+	cellFinder();
+	cells.forEach( (cell) => {
+		cell.addEventListener('mouseenter', () => {
+			cell.classList.add('colored');
+		});
 	});
-});
+};
 
+cellListener(); // Run for the first time
 
+const erase = () => {
+	cells.forEach( (cell) => {
+		cell.classList.remove('colored');
+	});
+};
 
+const resize = () => {
+	let gridSize = prompt('What size grid this time?');
+	etchPad.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr);`;
 
+	// Empty old cells before making the new ones
+	while (etchPad.hasChildNodes()) {
+		etchPad.removeChild(etchPad.firstChild);
+	}
 
+	divMaker(gridSize);
+	cellListener(); // Find all new cells and listen for mouse
+};
+
+const resizeButton = document.querySelector('button#resize');
+resizeButton.addEventListener('click', resize);
+
+const eraseButton = document.querySelector('button#erase');
+eraseButton.addEventListener('click', erase);
 
